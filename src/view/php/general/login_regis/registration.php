6d3b5f04851e85498fea8a1b26138085cd8f704a
gabdownbad <2227003@slu.edu.ph>
2025-02-03 09:49:27 +0800

    added design to registration.php

--- src/view/php/general/login_regis/registration.php
+++ src/view/php/general/login_regis/registration.php
@@ -109,7 +109,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600&display=swap" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.10.5/font/bootstrap-icons.min.css" rel="stylesheet">
-    <link rel="stylesheet" type="text/css" href="../../../../styles/registration.css">
+    <link rel="stylesheet" type="text/css" href="/src/view/styles/css/registration.css?v=1.0">
     <link rel="icon" type="png" href="../../../../../public/assets/img/SLU Logo.png">
     <style>
         .container {
