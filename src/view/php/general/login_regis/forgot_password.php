3708c09233f2b75c680d9ad8add5bee6574c30ba
gabdownbad <2227003@slu.edu.ph>
2025-02-13 15:53:49 +0800

    front-end of forgot password

--- src/view/php/general/login_regis/forgot_password.php
+++ src/view/php/general/login_regis/forgot_password.php
@@ -8,13 +8,8 @@
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600&display=swap" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.10.5/font/bootstrap-icons.min.css" rel="stylesheet">
-    <link rel="stylesheet" type="text/css" href="/src/view/styles/css/registration.css?v=1.0">
+    <link rel="stylesheet" type="text/css" href="/src/view/styles/css/forgot_password.css?v=1.0">
     <link rel="icon" type="png" href="../../../../../public/assets/img/SLU Logo.png">
-    <style>
-        .container {
-            padding-bottom: 60px;
-        }
-    </style>
 </head>
 
 <body>
@@ -22,7 +17,7 @@
     <div class="container py-5 mt-5">
         <div class="row justify-content-center">
             <div class="col-lg-6 col-md-8">
-                <div class="card shadow extra-large-card">
+                <div class="card shadow custom-card-size">
                     <div class="card-body">
                         <h1 class="text-center mb-4">Forgot Password</h1>
                         <p class="text-center">Enter your email address to receive a password reset link.</p>
