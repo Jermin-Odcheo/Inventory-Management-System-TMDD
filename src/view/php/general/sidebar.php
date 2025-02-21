b9bce7cd61e6eb74cab3ae246a28b021fb0a48bb
JERMIN <143476410+Jermin-Odcheo@users.noreply.github.com>
2025-02-21 10:46:22 +0800

    separated section for equipment module

--- src/view/php/general/sidebar.php
+++ src/view/php/general/sidebar.php
@@ -72,11 +72,34 @@ $role = isset($_SESSION['role']) ? $_SESSION['role'] : '';
                     </li>
                 </ul>
             </li>
-
             <!-- Equipment Management Dropdown -->
             <li class="dropdown-item">
                 <button class="dropdown-toggle" aria-expanded="false">
                     <i class="fa-solid fa-wrench"></i> Equipment Management
+                    <i class="fas fa-chevron-down dropdown-icon"></i>
+                </button>
+                <ul class="dropdown tree" aria-expanded="false">
+                    <li>
+                        <a href="../../modules/equipment_manager/equipment_details.php">
+                            Equipment Details
+                        </a>
+                    </li>
+                    <li>
+                        <a href="../../modules/equipment_manager/equipment_location.php">
+                            Equipment Location
+                        </a>
+                    </li>
+                    <li>
+                        <a href="../../modules/equipment_manager/equipment_status.php">
+                            Equipment Status
+                        </a>
+                    </li>
+                </ul>
+            </li>
+            <!-- Equipment Transaction Dropdown -->
+            <li class="dropdown-item">
+                <button class="dropdown-toggle" aria-expanded="false">
+                    <i class="fa-solid fa-wrench"></i> Equipment Transaction
                     <i class="fas fa-chevron-down dropdown-icon"></i>
                 </button>
                 <ul class="dropdown tree" aria-expanded="false">
@@ -95,186 +118,11 @@ $role = isset($_SESSION['role']) ? $_SESSION['role'] : '';
                             Receiving Report
                         </a>
                     </li>
-                    <li>
-                        <a href="../../modules/equipment_manager/equipment_details.php">
-                            Equipment Details
-                        </a>
-                    </li>
-                    <li>
-                        <a href="../../modules/equipment_manager/equipment_location.php">
-                            Equipment Location
-                        </a>
-                    </li>
-                    <li>
-                        <a href="../../modules/equipment_manager/equipment_status.php">
-                            Equipment Status
-                        </a>
-                    </li>
-                </ul>
             </li>
+        </ul>
+        </li>
 
-            <!-- Departments with nested dropdowns -->
-            <li class="dropdown-item">
-                <button class="dropdown-toggle" aria-expanded="false">
-                    <i class="fas fa-building"></i>
-                    Departments
-                    <i class="fas fa-chevron-down dropdown-icon"></i>
-                </button>
-                <ul class="dropdown tree" aria-expanded="false">
-                    <li>
-                        <a href="#"><span>Office of the President</span></a>
-                    </li>
-                    <li>
-                        <a href="#"><span>Office of the Executive Assistant to the President</span></a>
-                    </li>
-                    <li>
-                        <a href="#"><span>Office of the Internal Auditor</span></a>
-                    </li>
-                    <!-- Nested dropdown item -->
-                    <li class="dropdown-item">
-                        <button class="dropdown-toggle" aria-expanded="false">
-                            <span>Mission and Identity Cluster</span>
-                            <i class="fas fa-chevron-down dropdown-icon"></i>
-                        </button>
-                        <ul class="dropdown tree" aria-expanded="false">
-                            <li>
-                                <a href="#"><span>Office of the Vice President for Mission and Identity</span></a>
-                            </li>
-                            <li>
-                                <a href="#"><span>Center for Campus Ministry</span></a>
-                            </li>
-                            <li>
-                                <a href="#"><span>Community Extension and Outreach Programs Office</span></a>
-                            </li>
-                            <li>
-                                <a href="#"><span>St. Aloysius Gonzaga Parish Office</span></a>
-                            </li>
-                            <li>
-                                <a href="#"><span>Sunflower Child and Youth Wellness Center</span></a>
-                            </li>
-                        </ul>
-                    </li>
-                    <li class="dropdown-item">
-                        <button class="dropdown-toggle" aria-expanded="false">
-                            <span>Academic Cluster</span>
-                            <i class="fas fa-chevron-down dropdown-icon"></i>
-                        </button>
-                        <ul class="dropdown tree" aria-expanded="false">
-                            <li>
-                                <a href="#"><span>Office of the Vice President for Academic Affairs</span></a>
-                            </li>
-                            <li>
-                                <a href="#"><span>School of Accountancy, Management, Computing and Information Studies (SAMCIS)</span></a>
-                            </li>
-                            <li>
-                                <a href="#"><span>School of Advanced Studies (SAS)</span></a>
-                            </li>
-                            <li>
-                                <a href="#"><span>School of Engineering and Architecture (SEA)</span></a>
-                            </li>
-                            <li>
-                                <a href="#"><span>School of Law (SOL)</span></a>
-                            </li>
-                            <li>
-                                <a href="#"><span>School of Medicine (SOM)</span></a>
-                            </li>
-                            <li>
-                                <a href="#"><span>School of Nursing, Allied Health, and Biological Sciences Natural Sciences (SONAHBS)</span></a>
-                            </li>
-                            <li>
-                                <a href="#"><span>School of Teacher Education and Liberal Arts (STELA)</span></a>
-                            </li>
-                            <li>
-                                <a href="#"><span>Basic Education School (SLU BEdS)</span></a>
-                            </li>
-                        </ul>
-                    </li>
-                    <li class="dropdown-item">
-                        <button class="dropdown-toggle" aria-expanded="false">
-                            <span>External Relations, Media and Communications and Alumni Affairs</span>
-                            <i class="fas fa-chevron-down dropdown-icon"></i>
-                        </button>
-                        <ul class="dropdown tree" aria-expanded="false">
-                            <li>
-                                <a href="#"><span>Office of Institutional Development and Quality Assurance</span></a>
-                            </li>
-                            <li>
-                                <a href="#"><span>University Libraries</span></a>
-                            </li>
-                            <li>
-                                <a href="#"><span>University Registrar’s Office</span></a>
-                            </li>
-                            <li>
-                                <a href="#"><span>University Research and Innovation Center</span></a>
-                            </li>
-                        </ul>
-                    </li>
-                    <li class="dropdown-item">
-                        <button class="dropdown-toggle" aria-expanded="false">
-                            <span>Finance Cluster</span>
-                            <i class="fas fa-chevron-down dropdown-icon"></i>
-                        </button>
-                        <ul class="dropdown tree" aria-expanded="false">
-                            <li>
-                                <a href="#"><span>Office of the Vice President for Finance</span></a>
-                            </li>
-                            <li>
-                                <a href="#"><span>Asset Management and Inventory Control Office</span></a>
-                            </li>
-                            <li>
-                                <a href="#"><span>Finance Office</span></a>
-                            </li>
-                            <li>
-                                <a href="#"><span>Printing Operations Office</span></a>
-                            </li>
-                            <li>
-                                <a href="#"><span>Technology Management and Development Department (TMDD)</span></a>
-                            </li>
-                        </ul>
-                    </li>
-                    <li class="dropdown-item">
-                        <button class="dropdown-toggle" aria-expanded="false">
-                            <span>Administration Cluster</span>
-                            <i class="fas fa-chevron-down dropdown-icon"></i>
-                        </button>
-                        <ul class="dropdown tree" aria-expanded="false">
-                            <li>
-                                <a href="#"><span>Office of the Vice President for Administration</span></a>
-                            </li>
-                            <li>
-                                <a href="#"><span>Athletics and Fitness Center</span></a>
-                            </li>
-                            <li>
-                                <a href="#"><span>Campus Planning, Maintenance, and Security Department (CPMSD)</span></a>
-                            </li>
-                            <li>
-                                <a href="#"><span>Center for Culture and the Arts (CCA)</span></a>
-                            </li>
-                            <li>
-                                <a href="#"><span>Dental Clinic</span></a>
-                            </li>
-                            <li>
-                                <a href="#"><span>Guidance Center</span></a>
-                            </li>
-                            <li>
-                                <a href="#"><span>Human Resource Department (HRD)</span></a>
-                            </li>
-                            <li>
-                                <a href="#"><span>Students’ Residence Hall</span></a>
-                            </li>
-                            <li>
-                                <a href="#"><span>Medical Clinic</span></a>
-                            </li>
-                            <li>
-                                <a href="#"><span>Office for Legal Affairs (OLA)</span></a>
-                            </li>
-                            <li>
-                                <a href="#"><span>Office of Student Affairs (OSA)</span></a>
-                            </li>
-                        </ul>
-                    </li>
-                </ul>
-            </li>
+
         </ul>
     </nav>
 </div>
