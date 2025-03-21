7077f1e748a3a550320a139aee12e093f105792a
JERMIN <143476410+Jermin-Odcheo@users.noreply.github.com>
2025-03-21 08:25:37 +0800

    user_roles_management separated .js file and .css file

--- src/view/php/general/header.php
+++ src/view/php/general/header.php
@@ -17,10 +17,13 @@ $email = $_SESSION['email'];
     <title>Inventory Management System</title>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&family=Roboto+Mono:wght@300;500&display=swap"
           rel="stylesheet">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 
+    <!-- jQuery, Bootstrap CSS/JS, and Bootstrap Icons -->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css" rel="stylesheet">
+    
     <link rel="stylesheet" href="<?php echo BASE_URL; ?>src/view/styles/css/toast.css">
     <link rel="stylesheet" href="<?php echo BASE_URL; ?>src/view/styles/css/header_footer.css">
 
