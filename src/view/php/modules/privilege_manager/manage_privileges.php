1782d5145e98496fa1881c7d658386145dc2cd0c
JERMIN <143476410+Jermin-Odcheo@users.noreply.github.com>
2025-05-06 15:41:22 +0800

    em_archive.php add proper design action column

--- src/view/php/modules/privilege_manager/manage_privileges.php
+++ src/view/php/modules/privilege_manager/manage_privileges.php
@@ -26,8 +26,7 @@ $privileges = $stmt->fetchAll(PDO::FETCH_ASSOC);
     <meta name="viewport" content="width=device-width,initial-scale=1">
     <link rel="stylesheet" href="<?php echo BASE_URL; ?>src/view/styles/css/pagination.css">
     <link rel="stylesheet" href="<?php echo BASE_URL; ?>src/view/styles/css/manage_privileges.css">
-    <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet">
+ 
     <title>Manage Users</title>
 </head>
 
