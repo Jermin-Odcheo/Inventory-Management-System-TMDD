ea256c2a55a898db4d84e95e7beaa0817f4d0650
John Gabriel Pampo <2227003@slu.edu.ph>
2025-06-30 09:25:04 +0800

    Revert "fixed modal placement"
    This reverts commit be6b4f07826ca8dc8d04b252907ffdeb8ef7e32f.

--- src/view/php/modules/management/department_manager/department_management.php
+++ src/view/php/modules/management/department_manager/department_management.php
@@ -607,13 +607,6 @@ if (isset($_GET["q"])) {
             content: "\f0dd";
             opacity: 1;
         }
-
-        /* Ensure modals are not overlapped by the header */
-        #addDepartmentModal .modal-dialog,
-        #editDepartmentModal .modal-dialog,
-        #deleteDepartmentModal .modal-dialog {
-            margin-top: 120px !important; /* Adjusted to appear lower on the page */
-        }
     </style>
 
 </head>
